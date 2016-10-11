ccd2b6d1d1f68c7168ebf93314bfa05fd777868f
Sim Young-Bo <ybsim@khu.ac.kr>
2016-10-12 06:52:47 +0900

    Add scripts for emulation

---
+++ tools/scripts/hist.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*- 
+import os
+import sys
+import numpy
+import matplotlib.pyplot as plt
+
+def read_log(logfile_prefix):
+    l = list()
+    i = 0
+    try:
+        while(True):
+            logfile = '%s%04d.log' % (logfile_prefix, i)
+            with open(logfile, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    l.append((int)(line))
+            i = i+1
+    except:
+        pass
+
+    return l
+
+if __name__=='__main__':
+
+    if len(sys.argv) == 1:
+        print "Prefixes must be entered.(*MUST BE Directory name)"
+        sys.exit(1)
+
+    l = list()
+    labels = list()
+    
+    for arg in sys.argv[1:]:
+        l.append(read_log('./%s/' % arg))
+        labels.append(arg)
+
+    MAX = max(l[0])
+    MIN = min(l[0])
+    RANGE = numpy.arange(MIN-0.5, MAX+1.5)
+
+    # make hist
+    plt.hist(l, RANGE, histtype='bar', label=labels, alpha=0.5)
+
+    # fill text
+    plt.legend(loc='upper right')
+    #plt.title(r'Comparison the proposal with the AS')
+    plt.xlabel('The length of the path')
+    plt.ylabel('The number of received packets(ants)')
+
+    #
+    plt.grid(True)
+    plt.xticks(numpy.arange(MIN, MAX+1, 1.0))
+    locs, labels = plt.xticks()
+    plt.setp(labels, rotation=60)
+    #plt.yscale('log', nonposy='clip')
+
+    plt.savefig("hist.png", format="png")
+
+
+
+
+
+
---
+++ tools/scripts/stat.py
@@ -0,0 +1,58 @@
+import sys
+import os
+import numpy
+import operator
+
+def read_log(logfile):
+    l = list()
+    with open(logfile, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            l.append((int)(line))
+
+    return numpy.mean(l)
+
+def read_all_logs(logfile_prefix):
+    l = list()
+    nfile = 0
+    try:
+        while(True):
+            logfile = '%s%04d.log' % (logfile_prefix, nfile)
+            l.append(read_log(logfile))
+            nfile = nfile + 1
+    except:
+        pass
+
+    return l
+
+def mean_log(logfile_prefix):
+    all_logs = read_all_logs(logfile_prefix)
+    mean = list()
+    nlogs = len(all_logs)
+
+    for i in range(0, len(all_logs[0])):
+        s = 0
+        for log in all_logs:
+            s = s + log[i]
+        mean.append(s)
+
+    return [(i/float(nlogs))  for i in mean]
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        print "Prefixes must be entered.(*MUST BE Directory name)"
+        sys.exit(1)
+
+    for arg in sys.argv[1:]:
+        l = read_all_logs('./%s/' % arg)
+        min_index, min_value = min(enumerate(l), key=operator.itemgetter(1))
+        max_index, max_value = max(enumerate(l), key=operator.itemgetter(1))
+
+        print "Statistic info of %s   :\n" % arg,      \
+              "Total Average          : %f\n" % numpy.mean(l),  \
+              "Min Aver over logs     : %f\n" % min_value,      \
+              "Max Aver over logs     : %f\n" % max_value,      \
+              "File Index of Min Aver : %-4d\n" % min_index,      \
+              "File Index of Max Aver : %-4d\n\n" % max_index
+
