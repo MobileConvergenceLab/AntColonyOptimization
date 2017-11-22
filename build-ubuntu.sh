6c6da719beb51b6b91222810a9737761c0deb3d6
MSSeo <uiop0103@gmail.com>
2017-11-22 17:17:11 +0900

    Shell script update

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -4,6 +4,7 @@ apt-get install git -y
 apt-get install mininet -y
 apt-get install ovs -y
 apt-get install libpstream -y
+apt-get install libpstreams-dev -y
 apt-get install libglib2.0-dev -y
 
 make
